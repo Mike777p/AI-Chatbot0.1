5f74912ae80db94bff7c26607cb74bdefd3d0095
Mike777p <mjp7@duck.com>
2023-06-05 15:43:12 +0100

    Add db (Message creation)

--- backend/functions/db.py
+++ backend/functions/db.py
@@ -7,10 +7,22 @@ def get_recent_messages():
     file_name = "stored_data.json"
     learn_instruction = {
         "role":"system",
-        "content":"You are an extremly intelligent life coach who knows everything. If you need more information to answer the question accurately; you will formulate questions to ask the user. The user is called Mike and your purpose is to help him succeed in whatever he wants to do."
+        "content":"You are an extremly intelligent life coach who knows everything. If you need more information to answer a question accurately; you will formulate questions to ask the user. The user is called Mike and your purpose is to help him succeed and answer any question he has as accurately as possible."
     }
 
 # initilize  messages
 messages = []
 
 #add a random element
+x = random.uniform(1,10)
+
+if x < 5:
+    learn_instruction["content"] += " Your response will contain some light flirting"
+elif x < 9:
+    learn_instruction["content"] += " Your response will contain some humour"
+else:
+    learn_instruction["content"] += " Your response will contain a little moodiness" 
+
+
+# Append instruction to message
+messages.append(learn_instruction)
